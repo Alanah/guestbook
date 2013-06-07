9a27bf76e6500e9a4cf27f4c3228a6a297c78a87
Alanah <Lana383@yahoo.com>
2013-06-07 15:32:56 -0400

    guestbook app - chp 6 / finished

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '0f6c3947e2ca2969b05ddda27b2e677e17e0791e9f115964d3a431eeca0770570ca906a16269c3ad7b39b85e2a1605551f19989760f4025bb223d49b8e0b95a7'
