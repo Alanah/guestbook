1d5358a5b20397dcfc7b58b5d3c3745ff19e92c8
Alanah <Lana383@yahoo.com>
2013-06-10 15:34:50 -0400

    chp8_photo_update_error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130606170918) do
+ActiveRecord::Schema.define(:version => 20130610182304) do
 
   create_table "people", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130606170918) do
     t.time     "favorite_time"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "extension"
   end
 
 end
